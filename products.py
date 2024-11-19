78f69b602cff6f6b25fe114b8cc27827db92351d
Chang Sean <sean710021@gmail.com>
2024-11-19 21:05:16 +0800

    split line

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open('products.csv','r',encoding='utf-8') as f:
+	for line in f:
+		name,price = line.strip().split(',') #split切割完結果是清單
+		products.append([name,price])
+print(products)
+
+
+
+
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
